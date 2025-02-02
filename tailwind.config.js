d341408c5304fde525d6a00f025afb06edf94fc0
Sajidalrafi62 <sajid09114@gmail.com>
2025-02-03 00:13:43 +0600

    completed!

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,24 +6,3 @@ module.exports = {
     },
     plugins: [],
   }
-
-
-module.exports = {
-  content: [
-    "./src/**/*.{html,js}", // adjust path as necessary
-  ],
-  theme: {
-    extend: {
-      animation: {
-        fadeIn: "fadeIn 1.5s ease-out",
-      },
-      keyframes: {
-        fadeIn: {
-          "0%": { opacity: "0", transform: "translateY(-50px)" },
-          "100%": { opacity: "1", transform: "translateY(0)" },
-        },
-      },
-    },
-  },
-  plugins: [],
-}
